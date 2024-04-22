6953288f1692db0eacef1d8e458495bae0c6b63c
Kevin Simón Pérez <kevin.simon@students.salle.url.edu>
2024-04-22 11:53:59 +0200

    Fix: RealPreTabBar

--- app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/SingleFragmentActivity.java
+++ app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/SingleFragmentActivity.java
@@ -7,19 +7,19 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
 public class SingleFragmentActivity extends AppCompatActivity {
-    private TabBarFragment tabBarFragment;
+    /*private TabBarFragment tabBarFragment;
     protected Fragment createFragment() {
         return new TabBarFragment();
-    }
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singlefragmentactivity);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.singleFragmentActivity);
+        /*Fragment fragment = fragmentManager.findFragmentById(R.id.singleFragmentActivity);
         if (fragment == null) {
             fragment = createFragment();
             fragmentManager.beginTransaction().add(R.id.singleFragmentActivity, fragment).commit();
-        }
+        }*/
     }
 }
