72977910c1e9e80ca99b7b5b87a9ed7d60a78719
ericf <eric.faya@students.salle.url.edu>
2024-04-25 16:20:04 +0200

    Vista de tienda hecha con activity i listeners de botons

--- app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/TendaFragment.java
+++ app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/TendaFragment.java
@@ -1,64 +1,69 @@
 package edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TendaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TendaFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button bPokeball;
+    private Button bSuperball;
+    private Button bUltraball;
+    private Button bMasterball;
 
     public TendaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TendaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TendaFragment newInstance(String param1, String param2) {
-        TendaFragment fragment = new TendaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tenda, container, false);
+        View view = inflater.inflate(R.layout.fragment_tenda, container, false);
+
+        bPokeball = view.findViewById(R.id.button_pokeball);
+        bSuperball = view.findViewById(R.id.button_superball);
+        bUltraball = view.findViewById(R.id.button_ultraball);
+        bMasterball = view.findViewById(R.id.button_masterball);
+
+        bPokeball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bSuperball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bUltraball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bMasterball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }
