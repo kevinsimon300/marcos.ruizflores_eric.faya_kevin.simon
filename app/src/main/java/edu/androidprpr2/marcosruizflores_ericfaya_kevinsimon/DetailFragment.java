8104e3b5c4fda3dd156e66294e41186a8f8d56d3
Kevin Simón Pérez <kevin.simon@students.salle.url.edu>
2024-05-25 23:55:14 +0200

    Fix: BackDefaultPhotography

--- app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/DetailFragment.java
+++ app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/DetailFragment.java
@@ -64,6 +64,21 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
         ivPokedex = (ImageView) itemView.findViewById(R.id.ivPokemonBack);
         imageViewFront = (ImageView) itemView.findViewById(R.id.ivPokemonFront);
 
+        tvType = (TextView) itemView.findViewById(R.id.tvType); //El item view es internament el view holder,no es un objecte creat per nosaltres
+        tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
+
+        tvStats = (TextView) itemView.findViewById(R.id.tvStats); //El item view es internament el view holder,no es un objecte creat per nosaltres
+
+        tvNomPokedex.setText(pokedex.getName());
+        tvType.setText(pokedex.getTypes().toString());
+        tvDescription.setText(pokedex.getDescription());
+        //--- Importación de los botones
+        btnPokeball = (Button) itemView.findViewById(R.id.Button1_pokeball);
+        btnSuperball = (Button) itemView.findViewById(R.id.Button1_superball);
+        btnUltraball = (Button) itemView.findViewById(R.id.Button1_ultraball);
+        btnMasterball = (Button) itemView.findViewById(R.id.Button1_masterball);
+
+        tvSkills = (TextView) itemView.findViewById(R.id.tvHabilidades); //El item view es internament el view holder,no es un objecte creat per nosaltres
 
         Random random = new Random();
         int randomAbility = random.nextInt(4) + 1;
@@ -74,6 +89,7 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
             for (int i = 0; i < abilities.size(); i++){
                 if (abilities.get(i).getIs_hidden()){
                     ability = abilities.get(i).getName();
+                    tvSkills.setText(ability);
                 }
             }
         } else {
@@ -81,6 +97,7 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
             for (int i = 0; i < abilities.size(); i++){
                 if (!abilities.get(i).getIs_hidden()){
                     ability = abilities.get(i).getName();
+                    tvSkills.setText(ability);
                 }
             }
         }
@@ -93,29 +110,14 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
             Picasso.get().load(pokedex.getFront_shiny()).into(imageViewFront);
         } else {
             // Mostrar imágenes normales
-            Picasso.get().load(pokedex.getBackSprite()).into(ivPokedex);
-            Picasso.get().load(pokedex.getFrontSprite()).into(imageViewFront);
+            Picasso.get().load(pokedex.getBackImage()).into(ivPokedex);
+            Picasso.get().load(pokedex.getImageUrl()).into(imageViewFront);
         }
 
-        tvType = (TextView) itemView.findViewById(R.id.tvType); //El item view es internament el view holder,no es un objecte creat per nosaltres
-        tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
-
-        tvSkills = (TextView) itemView.findViewById(R.id.tvHabilidades); //El item view es internament el view holder,no es un objecte creat per nosaltres
-        tvStats = (TextView) itemView.findViewById(R.id.tvStats); //El item view es internament el view holder,no es un objecte creat per nosaltres
-
-        tvNomPokedex.setText(pokedex.getName());
-        tvType.setText(pokedex.getTypes().toString());
-        tvDescription.setText(pokedex.getDescription());
-        //--- Importación de los botones
-        btnPokeball = (Button) itemView.findViewById(R.id.Button1_pokeball);
-        btnSuperball = (Button) itemView.findViewById(R.id.Button1_superball);
-        btnUltraball = (Button) itemView.findViewById(R.id.Button1_ultraball);
-        btnMasterball = (Button) itemView.findViewById(R.id.Button1_masterball);
 
         btnPokeball.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: Aquí va el resto del código de la captura
                 Toast.makeText(getContext(), "Pokeball", Toast.LENGTH_SHORT).show();
             }
         });
