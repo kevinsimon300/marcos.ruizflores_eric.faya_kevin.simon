d94d94f7f704be3e0cfa472bf6fa00ea7c086a1e
Kevin Simón Pérez <kevin.simon@students.salle.url.edu>
2024-05-26 00:38:23 +0200

    Feat: HarcodedChecks

--- app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/DetailFragment.java
+++ app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/DetailFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.Ability;
+import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.Entrenador;
+import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.Item;
 import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.Pokemon;
 import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.PokemonDetail;
 
@@ -34,6 +37,7 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
     private TextView tvDescription;
     private TextView tvSkills;
     private TextView tvStats;
+    private TextView tvError;
     private Button btnPokeball;
     private Button btnSuperball;
     private Button btnUltraball;
@@ -42,6 +46,8 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
     private ImageView imageViewFront;
     private ImageView imageViewPokeball;//TODO
     private TextView tvNomPokedex;
+    private LinearLayout llBalls;
+    private LinearLayout llBalls2;
 
     public DetailFragment(Pokemon pokedex, ArrayList<Pokemon> pokedexes) {
         this.pokedex = pokedex;
@@ -58,6 +64,16 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
 
         View itemView= inflater.inflate(R.layout.fragment_detail, container, false);
         //ivPokedex = itemView.findViewById(R.id.ivImageFilm); // Initialize ivMovie here
+        ArrayList<Item> items = new ArrayList<>(); //Harcoded items
+        items.add(new Item("Pokeball", 200));
+        items.add(new Item("Superball", 600));
+        items.add(new Item("Ultraball", 1200));
+        items.add(new Item("Masterball", 5000));
+        ArrayList<PokemonDetail> pokemonsTrainer = new ArrayList<>(); //Harcoded pokemons
+        PokemonDetail pokemonDetail = new PokemonDetail("Bulbasaur", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", new ArrayList<String>(), "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.", new ArrayList<Ability>(), null);
+        pokemonsTrainer.add(pokemonDetail);
+        Entrenador entrenador = new Entrenador("Ash", 100,items, pokemonsTrainer);
+        boolean[] checks = checkIfPokemonIsCaptured(entrenador);
 
         tvNomPokedex = (TextView) itemView.findViewById(R.id.tvNamePokemon); //El item view es internament el view holder,no es un objecte creat per nosaltres
 
@@ -69,6 +85,9 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
 
         tvStats = (TextView) itemView.findViewById(R.id.tvStats); //El item view es internament el view holder,no es un objecte creat per nosaltres
 
+        tvError = (TextView) itemView.findViewById(R.id.tvError);
+        llBalls = (LinearLayout) itemView.findViewById(R.id.llCapturaBall);
+
         tvNomPokedex.setText(pokedex.getName());
         tvType.setText(pokedex.getTypes().toString());
         tvDescription.setText(pokedex.getDescription());
@@ -79,6 +98,14 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
         btnMasterball = (Button) itemView.findViewById(R.id.Button1_masterball);
 
         tvSkills = (TextView) itemView.findViewById(R.id.tvHabilidades); //El item view es internament el view holder,no es un objecte creat per nosaltres
+
+        if (checks[0]){
+            tvError.setText("You have already captured 6 pokemons");
+            llBalls.setVisibility(View.GONE);
+        } else if (checks[1]){
+            tvError.setText("You have already captured this pokemon");
+            llBalls.setVisibility(View.GONE);
+        }
 
         Random random = new Random();
         int randomAbility = random.nextInt(4) + 1;
@@ -148,5 +175,19 @@ public class DetailFragment extends Fragment {//Que es creei el on create,el fra
 
         tvStats.setText("hp: "+pokedex.getStat0() + " \nattack: " + pokedex.getStat1() + " \ndefense: " + pokedex.getStat2()+ " \nspecial-attack: " + pokedex.getStat3() + " \nspecial-defense: " + pokedex.getStat4() + " \nspeed: " + pokedex.getStat5());
         return itemView;
+    }
+
+    private boolean[] checkIfPokemonIsCaptured(Entrenador entrenador) {
+        boolean[] captured = new boolean[2];
+        if (entrenador.getlPokedex().size() < 6) {
+            captured[0] = true;
+        } else {
+            for (PokemonDetail pokemonDetail : entrenador.getlPokedex()) {
+                if (pokemonDetail.getNamePokemon().equals(pokedex.getName())){
+                    captured[1] = true;
+                }
+            }
+        }
+        return captured;
     }
 }
