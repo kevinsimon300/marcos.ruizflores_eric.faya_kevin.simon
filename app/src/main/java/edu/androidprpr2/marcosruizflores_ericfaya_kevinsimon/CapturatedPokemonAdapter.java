88f6471c32229bb640910fb9555f5f6dd18ecae6
Kevin Simón Pérez <kevin.simon@students.salle.url.edu>
2024-06-30 12:26:58 +0200

    Fix: DeletePokemon

--- app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/CapturatedPokemonAdapter.java
+++ app/src/main/java/edu/androidprpr2/marcosruizflores_ericfaya_kevinsimon/CapturatedPokemonAdapter.java
@@ -1,6 +1,7 @@
 package edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,17 +16,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.model.PokemonCapturado;
-import edu.androidprpr2.marcosruizflores_ericfaya_kevinsimon.peristence.SharedPreferencesDao;
 
 public class CapturatedPokemonAdapter extends RecyclerView.Adapter<CapturatedPokemonAdapter.PokemonViewHolder> {
 
     private List<PokemonCapturado> pokemonList;
     private Context context;
     private static final String TAG = "CapturedPokemonAdapter";
+    private static final String PREFERENCES_FILE = "trainer_prefs";
+    private static final String KEY_CAPTURED_POKEMON = "captured_pokemon";
 
     public CapturatedPokemonAdapter(List<PokemonCapturado> pokemonList, Context context) {
         this.pokemonList = pokemonList;
@@ -57,7 +63,7 @@ public class CapturatedPokemonAdapter extends RecyclerView.Adapter<CapturatedPok
                 int adapterPosition = holder.getAdapterPosition();
                 if (adapterPosition != RecyclerView.NO_POSITION) {
                     pokemonList.remove(adapterPosition);
-                    PokemonCapturado.savePokemonList(context, new ArrayList<>(pokemonList));
+                    savePokemonListToPreferences(context, new ArrayList<>(pokemonList));
                     notifyItemRemoved(adapterPosition);
                 }
             } else {
@@ -74,6 +80,25 @@ public class CapturatedPokemonAdapter extends RecyclerView.Adapter<CapturatedPok
     public void updatePokemonList(List<PokemonCapturado> newPokemonList) {
         this.pokemonList = newPokemonList;
         notifyDataSetChanged();
+    }
+
+    private void savePokemonListToPreferences(Context context, List<PokemonCapturado> pokemonList) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        JSONArray jsonArray = new JSONArray();
+        for (PokemonCapturado pokemon : pokemonList) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("name", pokemon.getName());
+                jsonObject.put("frontImage", pokemon.getFrontImage());
+                jsonObject.put("capturedPokeballImage", pokemon.getCapturedPokeballImage());
+                jsonArray.put(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        editor.putString(KEY_CAPTURED_POKEMON, jsonArray.toString());
+        editor.apply();
     }
 
     public static class PokemonViewHolder extends RecyclerView.ViewHolder {
